0a5f777d7cff78abdae3f0732290e16a2e8e54ab
GrillinGorilla <schaefd2@gmail.com>
2022-12-13 14:47:21 -0500

    just gotta finish the status messages now

--- js/js.js
+++ js/js.js
@@ -13,3 +13,227 @@ function c9ColorLoop()
 			$("#changeTheLightsHeader").css('color', '#E83B3B');
 		});
 }
+
+function c9MultiColor()
+{
+	$.post("https://449.duckdns.org/api/webhook/-0HdVk5rTDNIhkHo_VE-QAtcY", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function christmasAurora()
+{
+	$.post("https://449.duckdns.org/api/webhook/-0HdVk5rTDNIhkHo_VE-QAtcY", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function christmasPlasma()
+{
+	$.post("https://449.duckdns.org/api/webhook/-i1uoU-lSxGsaBPaompsQWPuK", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function christmasSlowTwinkle()
+{
+	$.post("https://449.duckdns.org/api/webhook/-GA6X0ZJhs_17PV_HA83KRWp4", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function gobleGlitter()
+{
+	$.post("https://449.duckdns.org/api/webhook/-SHaXERWz9Qgb3Eu872iBZYCc", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function gobleGlitter2()
+{
+	$.post("https://449.duckdns.org/api/webhook/-xX6NZfaVWtIATQgA_b2yi6B1", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function solidBlue()
+{
+	$.post("https://449.duckdns.org/api/webhook/-pw1v1sLCVANTp2H3WTMrH3__", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function solidGreen()
+{
+	$.post("https://449.duckdns.org/api/webhook/-pw1v1sLCVANTp2H3WTMrH3__", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function solidOrange()
+{
+	$.post("https://449.duckdns.org/api/webhook/-n_KGidx2jB9WEFGc8S6uhUVP", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function solidPink()
+{
+	$.post("https://449.duckdns.org/api/webhook/-CTQK-kXueRM7T1SsbxNKIpfB", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function solidPurple()
+{
+	$.post("https://449.duckdns.org/api/webhook/-BdndD3A4NB8HEeMaY1uKm801", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function solidRed()
+{
+	$.post("https://449.duckdns.org/api/webhook/-5jBEo2k13qX0xnpMdDwbM6NJ", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function solidTeal()
+{
+	$.post("https://449.duckdns.org/api/webhook/-xQA6l73ohnZ-E-YWNbSSWmH8", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function solidWarmWhite()
+{
+	$.post("https://449.duckdns.org/api/webhook/-uFUGf7zwoWaXtu1KroJPs2BU", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function warmWhiteDarkShadows()
+{
+	$.post("https://449.duckdns.org/api/webhook/-E8Ux658X0Km6xM9Kr2E7MWaI", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
+
+function warmWhiteFairyTwinkle()
+{
+	$.post("https://449.duckdns.org/api/webhook/-m6pCphrd59uIHRXRppEJbP0u", 
+		  function(data,status){$.toast({
+				text: "The house should be flashing colors now.",
+				heading: status,
+				position: 'top-right',
+				bgColor: '#2ecc71',
+				textColor: '#fff',
+			  });
+			$("#changeTheLightsHeader").css('color', '#E83B3B');
+		});
+}
